e969907838af82fe7ba2f8f87cb4300836989d24
Graham Hall <ghall89@me.com>
2021-03-21 12:49:11 -0400

    added inquirer

--- app.js
+++ app.js
@@ -1,23 +1,21 @@
 const fs = require('fs');
+const inquirer = require('inquirer')
 const generatePage = require('./src/page-template');
 
-const profileDataArgs = process.argv.slice(2, process.argv.length);
+inquirer
+	.prompt([
+		{
+			type: 'input',
+			name: 'name',
+			message: 'What is your name?'
+		}
+	])
+	.then(answers => console.log(answers));
 
-const [name, github] = profileDataArgs;
-
-fs.writeFile('index.html', generatePage(name, github), err => {
-	if (err) throw err;
-	
-	console.log('Portfolio complete! Check out index.html to see the output!');
-})
-
-// const printProfileData = profileDataArr => {
-// 	for (let i = 0; i < profileDataArr.length; i++) {
-// 		console.log(profileDataArr[i]);
-// 	}
+// const pageHTML = generatePage(name, github);
+// 
+// fs.writeFile('index.html', pageHTML, err => {
+// 	if (err) throw err;
 // 	
-// 	profileDataArr.forEach(profileItem => console.log(profileItem));
-// 
-// };
-// 
-// printProfileData(profileDataArgs);
+// 	console.log('Portfolio complete! Check out index.html to see the output!');
+// });
